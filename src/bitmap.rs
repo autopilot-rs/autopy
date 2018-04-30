4ebbb5076544444238be7c7304d84c05156a1f12
Michael Sanders <michael.sanders@fastmail.com>
2018-04-30 12:01:48 -0700

    Add bytes implementation for bitmaps

--- src/bitmap.rs
+++ src/bitmap.rs
@@ -39,6 +39,60 @@ impl PyObjectProtocol for Bitmap {
     }
 }
 
+#[py::proto]
+impl pyo3::class::PyBufferProtocol for Bitmap {
+    // From
+    // https://github.com/PyO3/pyo3/blob/14ab12/tests/test_buffer_protocol.rs#L18
+    fn bf_getbuffer(&self, view: *mut ffi::Py_buffer, flags: c_int) -> PyResult<()> {
+        use pyo3::*;
+        use std::os::raw::{c_int, c_void};
+        use std::ptr;
+
+        if view.is_null() {
+            return Err(PyErr::new::<exc::BufferError, _>("View is null"));
+        }
+
+        unsafe {
+            (*view).obj = ptr::null_mut();
+        }
+
+        if (flags & ffi::PyBUF_WRITABLE) == ffi::PyBUF_WRITABLE {
+            return Err(PyErr::new::<exc::BufferError, _>("Object is not writable"));
+        }
+
+        let bytes = &self.bitmap.image.raw_pixels();
+
+        unsafe {
+            (*view).buf = bytes.as_ptr() as *mut c_void;
+            (*view).len = bytes.len() as isize;
+            (*view).readonly = 1;
+            (*view).itemsize = 1;
+
+            (*view).format = ptr::null_mut();
+            if (flags & ffi::PyBUF_FORMAT) == ffi::PyBUF_FORMAT {
+                let msg = ::std::ffi::CStr::from_ptr("B\0".as_ptr() as *const _);
+                (*view).format = msg.as_ptr() as *mut _;
+            }
+
+            (*view).ndim = 1;
+            (*view).shape = ptr::null_mut();
+            if (flags & ffi::PyBUF_ND) == ffi::PyBUF_ND {
+                (*view).shape = (&((*view).len)) as *const _ as *mut _;
+            }
+
+            (*view).strides = ptr::null_mut();
+            if (flags & ffi::PyBUF_STRIDES) == ffi::PyBUF_STRIDES {
+                (*view).strides = &((*view).itemsize) as *const _ as *mut _;
+            }
+
+            (*view).suboffsets = ptr::null_mut();
+            (*view).internal = ptr::null_mut();
+        }
+
+        Ok(())
+    }
+}
+
 #[py::methods]
 impl<'a> Bitmap {
     /// Saves image to absolute path in the given format. The image type is
