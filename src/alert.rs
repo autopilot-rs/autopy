2a5c62266eb1a8cd3a1413b4933ec16135972a27
Michael Sanders <michael.sanders@fastmail.com>
2020-01-06 17:46:12 -0800

    Adjust capitalization and update copyright year

--- src/alert.rs
+++ src/alert.rs
@@ -1,7 +1,7 @@
-// Copyright 2018, 2019 Michael Sanders
+// Copyright 2018, 2019, 2020 Michael Sanders
 //
 // Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
-// https://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// https://apache.org/licenses/LICENSE-2.0> or the MIT License <LICENSE-MIT or
 // https://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
